e96f5f94603b682979aca52f17b0f1475aea97ce
lucia cicchelli <luciafcicchelli@gamail.com>
2024-10-17 18:31:08 +0000

    modificaciones

---
+++ puntob.sh
@@ -0,0 +1,14 @@
+ sudo mkfs.ext4 /dev/sdc2
+ sudo mkfs.ext4 /dev/sdc3
+ sudo mkfs.ext4 /dev/sdc4
+ sudo mkfs.ext4 /dev/sdc1
+
+ sudo mkdir /mnt/sdc1  
+ sudo mkdir /mnt/sdc2
+ sudo mkdir /mnt/sdc3
+ sudo mkdir /mnt/sdc4
+
+ sudo mount /dev/sdc1 /mnt/sdc1
+ sudo mount /dev/sdc2 /mnt/sdc2
+ sudo mount /dev/sdc3 /mnt/sdc3
+ sudo mount /dev/sdc4 /mnt/sdc4
